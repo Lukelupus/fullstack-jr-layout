965685231b29c334315678ee184cc4cf2a98f798
Lucas Boti <luke.boti.lorken@gmail.com>
2022-08-05 11:09:34 -0300

    added routes folder with standart route.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
+import Layout from "./routes/routes.js"
+
 
 dotenv.config();
 
@@ -9,6 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
 const PORT = process.env.PORT || 8000
+
+
+app.use("/", Layout)
 
 
 app.listen(PORT, () => {
