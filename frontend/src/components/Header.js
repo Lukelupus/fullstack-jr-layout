e10152bef32a22b67d8a1f616224fa0aef534194
Lucas Boti <luke.boti.lorken@gmail.com>
2022-08-05 15:18:57 -0300

    added navbar with in8 icon and anchor tags

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,32 @@
 import React from "react"
 
+
 function Header() {
-    return(
-        <>
+    return(<>
+    <div className="nav-container">
+        <nav className="navbar">
+            <ul>
+                <li className="logo">
+                    <img src="logo-in8-dev.svg" alt="logo"></img>
+                </li>
+                <li className="nav-item">
+                    <a href="#cadastro">Cadastro</a>
+                </li>
+                <li className="nav-item">
+                <a href="#lista">Lista</a>
+                </li>
+                <li className="nav-item">
+                <a href="#sobre">Sobre Mim</a>
+                </li>
+            </ul>
+
+        </nav>
             
-        </>
+    </div>
+            
+        
+    </>
     )
 }
+
+export default Header
