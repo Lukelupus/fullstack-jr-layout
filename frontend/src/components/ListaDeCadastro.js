f495e533338dc142aef19b58f3a79f14586590f8
Lucas Boti <luke.boti.lorken@gmail.com>
2022-08-06 15:18:01 -0300

    added axios package to handle HTTP requests

--- frontend/src/components/ListaDeCadastro.js
+++ frontend/src/components/ListaDeCadastro.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
+const data = [
+    {
+        name: "",
+        email:"",
+        birth:"",
+        tel:""
+    },
+]
+
 
 function ListaDeCadastro() {
     return(
@@ -8,11 +17,21 @@ function ListaDeCadastro() {
                 <h2 className="list-title">Lista de Cadastro</h2>
                 <table>
                     <tr>
+                        <th className='table-heading'></th>
                         <th className="table-heading">Nome</th>
                         <th className="table-heading">Email</th>
                         <th className="table-heading">Nascimento</th>
                         <th className="table-heading">Telefone</th>
                     </tr>
+                    {data.map((val, key) => {
+                        <tr key={key}>
+                            <td>{val.name}</td>
+                            <td>{val.email}</td>
+                            <td>{val.birth}</td>
+                            <td>{val.tel}</td>
+
+                        </tr>
+                    })}
                 </table>
             </div>
         </>
